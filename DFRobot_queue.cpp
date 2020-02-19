f9410d1bc439041b3d7cec91f34630fe85bffdfa
pkx890 <1264657970@qq.com>
2020-02-19 19:44:17 +0800

    description

--- DFRobot_queue.cpp
+++ DFRobot_queue.cpp
@@ -7,9 +7,6 @@ void cuappEnqueue(uint8_t *pbuf, uint16_t len, uint16_t conn_id){
     struct sQueueData *p;
     p = (struct sQueueData*)malloc(sizeof(struct sQueueData)+len+1);
     if(p == NULL){
-/* 		Serial.print("Memory==");Serial.println(getFreeMemory());
-		Serial.print("pbuf==");Serial.println((char*)pbuf); */
-		//Serial.println("==================");
         free(p);
         return;
     }
@@ -25,9 +22,6 @@ void cuappEnqueue(uint8_t *pbuf, uint16_t len, uint16_t conn_id){
     p->handle = conn_id;
     memset(p->data,'\0',len+1);
     memcpy(p->data, pbuf, len);
-	//Serial.print("conn_id==");Serial.println(conn_id);
-	//Serial.print("p->data==");Serial.println((char*)p->data);
-	//Serial.println((char*)(getQueueHead())->data);
 }
 
 struct sQueueData *cuappDequeue(void){
